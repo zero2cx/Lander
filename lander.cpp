e8253c288ea982f17bda4cef4fede0c7a2ad45dd
Richard Pavlov <richardpavlov@hotmail.com>
2016-11-21 20:40:15 +0000

    Fixed borders(dirty af)

--- lander.cpp
+++ lander.cpp
@@ -102,6 +102,8 @@ int main(){
 				mvprintw(7,4,"wtf:%i", wtf);
 				mvprintw(8,4,"kbhit ch:%i", chKBHIT);
 				auto current_time = std::chrono::high_resolution_clock::now();
+				mvprintw(0,w.ws_col - 3,"|");
+				mvprintw(1,w.ws_col - 3,"|");
 				mvprintw(0, w.ws_col - 12, "Time:%i", std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count());
 			}
 			if ( (startPoint == randomXn || startPoint + 1 == randomXn || startPoint + 2 == randomXn) && (rockstart == w.ws_row - 3) ){
