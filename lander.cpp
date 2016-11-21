ee2d5da59882f40b75e2a6f661394d7e26aa5273
Huetagoyena <facundohuetagoyena1@gmail.com>
2016-11-21 20:19:01 +0100

    Fixed Game Over screen

--- lander.cpp
+++ lander.cpp
@@ -191,9 +191,9 @@ int main(){
 	}
 	GOVER:refresh();
 	clear();
-	mvprintw(10,10,"GAME OVER");
+	mvprintw(w.ws_row/2,w.ws_col/2,"GAME OVER");
 	refresh();
-	getch();
+	sleep_ms(3000);
 	endwin();
 	return 0;
 }
