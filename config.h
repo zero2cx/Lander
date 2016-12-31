4c1e692b148115e2d6c1a7cd0be8552488cf9490
Huetagoyena <facundohuetagoyena1@gmail.com>
2016-12-31 15:11:18 +0100

    Added comments and Color option

--- config.h
+++ config.h
@@ -1,10 +1,11 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define KEY_MOVE_LEFT 'z'
+#define KEY_MOVE_LEFT 'z'		// MOVEMENT CONTROLS
 #define KEY_MOVE_RIGHT 'c'
 #define KEY_SHOOT 'x'
 
-#define debugGraph false
+#define debugGraph false		// CONSOLE (MAY CAUSE SOME GLITCHES)
+#define colorsEnabled true	  // COLORS
 
 #endif
